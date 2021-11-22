2d6221e64eb1314579e841fc77fdc79e74bec33c
Dilpreet1996 <dilpreetsingh.ds277@gmail.com>
2021-11-22 07:46:32 -0500

    aboutDone

--- routes/index.js
+++ routes/index.js
@@ -34,10 +34,31 @@ router.get('/about', (req,res,next) =>{
     Contact: 'Contact',
     IMAGE: 'https://raw.githubusercontent.com/Dilpreet1996/MyPortfolio/master/assests/image.jpg',
     ALT: "dil's image",
-  
-    about:'About Me'
+    carrerObjective:'To enhance my knowledge and skills by working with your company and to use that skills for the growth and development of your company.',
+    about:'About Me',
+    education: 'I have Diploma and B.Tech from Punjab Technical Board and BCET University India, Moreover currently i am doing Post-Graduate Diploma in Mobile Solutions Development from Conestoga College at Waterloo, Canada',
+    strength:'My Hobbies are Running, Traveling and Coding while my Interest is to learn new things all the times and in term of my strength which is  Self Motivated, Hardworker,  Honest.',
+    
    
-})
+});
+});
+router.get('/contacts',(req,res,next) => {
+  res.render('contacts',
+  { 
+    Home: 'Home', 
+    Projects: 'Projects',
+    AboutMe: 'AboutMe',
+    LOGO: 'Dilpreet Singh |',
+    Contact: 'Contact',
+    IMAGE: 'https://raw.githubusercontent.com/Dilpreet1996/MyPortfolio/master/assests/image.jpg',
+    ALT: "dil's image",
+    location: "H.no 393, Waterloo",
+    about:'About Me',
+    Email:'dilpreetsingh.ds277@gmail.com',
+    number:'+16476171448',
+    sub:'Submit!'
+  });
+
 });
 
 module.exports = router;
